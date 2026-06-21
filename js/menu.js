11b13f4655c4d4211c98f488f240215cece9e6cc
Gabrielle Rodrigues <silva111gabi16@gmail.com>
2026-06-21 02:40:44 -0300

    correçoes html e css

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,8 @@ let menuButton = document.getElementById("menuMobile");
 
 if (menuButton && menu) {
     menuButton.addEventListener("click", () => {
+        let isExpanded = menuButton.getAttribute("aria-expanded") === "true";
         menu.classList.toggle("ativo");
+        menuButton.setAttribute("aria-expanded", String(!isExpanded));
     });
 }
